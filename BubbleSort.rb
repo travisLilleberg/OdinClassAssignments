35d1c3adcf5464786273e3b4676659de41e51f6a
Travis Lilleberg <travis.lilleberg@civicactions.com>
2025-10-31 16:40:20 -0400

    Made bubblesort more rubyified.

--- BubbleSort.rb
+++ BubbleSort.rb
@@ -4,9 +4,7 @@ def bubble_sort(arr)
     i = 0
     while(i < j)
       if(arr[i] > arr[i+1])
-        tmp = arr[i]
-        arr[i] = arr[i+1]
-        arr[i+1] = tmp
+        arr[i], arr[i+1] = arr[i+1], arr[i]
       end
       i += 1
     end
