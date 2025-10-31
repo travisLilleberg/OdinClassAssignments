bd7f869d87eb08bb49afd007350da0f62779c259
Travis Lilleberg <travis.lilleberg@civicactions.com>
2025-10-31 16:35:25 -0400

    Added bubble sort

---
+++ BubbleSort.rb
@@ -0,0 +1,22 @@
+def bubble_sort(arr)
+  j = arr.length - 1
+  while(j > 0)
+    i = 0
+    while(i < j)
+      if(arr[i] > arr[i+1])
+        tmp = arr[i]
+        arr[i] = arr[i+1]
+        arr[i+1] = tmp
+      end
+      i += 1
+    end
+    j -= 1
+  end
+
+  arr
+end
+
+arr = [*1..20].shuffle
+p arr
+p bubble_sort(arr)
+#[0,2,2,3,4,78]
